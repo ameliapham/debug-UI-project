9137afb4d99a0944116b0360120ed72335c042e9
ameliapham <ptt.huong.8895@gmail.com>
2023-12-07 19:20:19 +0100

    Set up project, add style.css and fix bug interface

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 import * as THREE from 'three'
 
-console.log("Hello")
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1,1,1,2,2,2)
+const material = new THREE.MeshBasicMaterial({color: 'pink'})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+camera.position.z = 3
+scene.add(camera)
+
+window.addEventListener('resize', () => {
+    // Update camera 
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    // Update renderer 
+    renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({canvas: canvas})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
