cf4833c1852ff2913b36bfb5e75c8a7885f4f384
ameliapham <ptt.huong.8895@gmail.com>
2023-12-08 18:43:17 +0100

    Add some tweaks GUI

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import GUI from 'lil-gui'
 
 // Debug
 const gui = new GUI()
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -12,11 +13,30 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
+
 const geometry = new THREE.BoxGeometry(1,1,1,2,2,2)
-const material = new THREE.MeshBasicMaterial({color: 'pink'})
+const material = new THREE.MeshBasicMaterial({color: debugObject.color = "#eea0dd"
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+gui
+    .add(mesh.scale, 'x')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('scaleX')
+
+gui
+    .add(mesh, 'visible')
+
+gui 
+    .add(material, 'wireframe')
+
+gui
+    .addColor(material, 'color')
+
+ 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
 camera.position.z = 3
@@ -43,7 +63,6 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 const animation = () =>{
     // Controls Update
     controls.update()
-    console.log("Hello")
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(animation)
